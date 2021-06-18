4548123e7a65bee210e19f04bbbb59c5acb735df
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-18 14:14:34 -0300

    commit inicial

---
+++ app/src/main/java/com/jefmelo/autenticationforsms/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jefmelo.autenticationforsms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
